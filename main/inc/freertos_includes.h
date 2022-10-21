a4cceeaeda06dac06e30de621973558a978b98e9
Benoit Florimond <benoitflorimond50@gmail.com>
2022-10-21 15:40:42 +0200

    add fwk submodule and some clean
    Signed-off-by: Benoit Florimond <benoitflorimond50@gmail.com>

---
+++ main/inc/freertos_includes.h
@@ -0,0 +1,17 @@
+/**
+* ****************************************************************************
+* freertos_includes.h
+* ****************************************************************************
+*/
+
+#ifndef __FREERTOS_INCLUDES_H__
+#define __FREERTOS_INCLUDES_H__
+
+/* Includes ******************************************************************/
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#endif /* __FREERTOS_INCLUDES_H__ */
