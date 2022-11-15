e6925b352acbb3c9fbd7d7c5764d564bff53c8c0
Benoit Florimond <benoitflorimond50@gmail.com>
2022-11-15 14:56:01 +0100

    done but not tested
    Signed-off-by: Benoit Florimond <benoitflorimond50@gmail.com>

--- main/main.c
+++ main/main.c
@@ -14,13 +14,16 @@
 #include "position_app.h"
 #include <driver/gpio.h>
 #include <esp_log.h>
+#include "hbridge_dir_hil.h"
 
 /* Private Defines & Macros **************************************************/
 
 #define TASK_TAG               ("MAIN")
 #define LED_GPIO_NUM           (GPIO_NUM_21)
 #define PWMA_GPIO_NUM          (GPIO_NUM_22)
-#define PWMB_GPIO_NUM          (GPIO_NUM_23)
+#define DIR_GPIO_NUM          (GPIO_NUM_23)
+#define MOTOR1                 (0)
+#define MOTOR2                 (1)
 #define MAIN_INFO(fmt, ...)    ESP_LOGI(TASK_TAG, fmt, ##__VA_ARGS__)
 #define MAIN_ERROR(fmt, ...)   ESP_LOGE(TASK_TAG, fmt, ##__VA_ARGS__)
 #define MAIN_WARNING(fmt, ...) ESP_LOGW(TASK_TAG, fmt, ##__VA_ARGS__)
@@ -28,32 +31,46 @@
 /* Private types definition **************************************************/
 
 /* Private variables *********************************************************/
+static brushedMotorHandle_t _motorHandle = NULL;
 
 /* Private prototypes ********************************************************/
 
+static void * _motorInit(uint32_t freq, void* args);
+
 /* Private Functions *********************************************************/
+
+static void * _motorInit(uint32_t freq, void* args)
+{
+    if(args == (void*)MOTOR1){
+        return HBDHIL_initIos(PWMA_GPIO_NUM, DIR_GPIO_NUM, freq);
+    }
+
+    return NULL;
+}
 
 /* Public Functions **********************************************************/
 
 void app_main()
 {
     uint8_t ledHandle = 0;
-    brushedMotorHandle_t motorHandle = NULL;
 
     if (LEDDRV_init() != ESP_OK) {
         MAIN_ERROR("LED driver init failed");
     }
 
-    motorHandle = BMDRV_addMotor(PWMA_GPIO_NUM, PWMB_GPIO_NUM, 44000);
-    BMDRV_setSpeed(motorHandle, 50.0);
+    _motorHandle = BMDRV_addMotor(3900, _motorInit, (void*)MOTOR1, (brushedMotorSetDutyCycleCbk_t)HBDHIL_setDutyCycle);
+    if(_motorHandle == NULL){
+        MAIN_ERROR("Motor init failed");
+    }
 
-        if (POSAPP_init() != ESP_OK)
-    {
+    if (POSAPP_init() != ESP_OK) {
         MAIN_ERROR("Position app init failed");
     }
 
+    ledHandle = LEDDRV_registerLed(LED_GPIO_NUM, LED_NO_PIN, LED_NO_PIN);
+
     OSUTILS_setSystemReady();
 
-    ledHandle = LEDDRV_registerLed(LED_GPIO_NUM, LED_NO_PIN, LED_NO_PIN);
     LEDDRV_setLedBlinking(ledHandle, 0xFF0000, true, 100, 500, BLINK_CONTINIOUSLY);
+    BMDRV_setSpeed(_motorHandle, 50.0);
 }
